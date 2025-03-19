ee7777b6b764b570103730f12e21513dc73f001c
PierrickDEN03 <pierrick.dennemont@gmail.com>
2025-03-19 18:28:58 +0100

    Project file, commit de taille relativement moyenne

---
+++ GiFinder/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  GiFinder
+//
+//  Created by Loic Babolat on 19/03/2025.
+//
+
+import Foundation
+
+let package = Package(
+    dependencies: [
+    .package(url: "https://github.com/kaishin/Gifu.git", from: "3.2.2")
+    ]
+)
